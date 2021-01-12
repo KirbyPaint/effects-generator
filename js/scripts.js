136b5e6718281b097ef90996f9c7b1ab80668991
KirbyPaint <porter.ashley13@gmail.com>
2021-01-12 15:45:15 -0800

    change minor effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
+function rng() {
+  return 40;
+}
+
 $(document).ready(function() {
   $("form#form").submit(function(event) {
     $("#text").show();
+
+    const randomNumber = rng();
+
+    $("#text").prepend(randomNumber);
 
     event.preventDefault();
   });
