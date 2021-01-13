e3a153cadece533a3c09c4b1b744cc73b2f7793e
KirbyPaint <porter.ashley13@gmail.com>
2021-01-12 16:53:33 -0800

    add minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-function rng() {
-  return 40;
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 $(document).ready(function() {
   $("form#form").submit(function(event) {
     $("#text").show();
 
-    const randomNumber = rng();
+    const randomNumber = getRandomInt(40);
 
-    $("#text").prepend(randomNumber);
+    $("#text").prepend(randomNumber + " " + "<br>");
 
     event.preventDefault();
   });
