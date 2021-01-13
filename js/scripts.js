1b20746a44441b6b1db493bf959859c4e2930107
KirbyPaint <porter.ashley13@gmail.com>
2021-01-12 17:54:42 -0800

    restructure array

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,14 @@ function getRandomInt(max) {
 
 $(document).ready(function() {
   $("form#form").submit(function(event) {
+    const g_melee = ["2 For 1<br>Target suffers maximum damage and you deal normal damage to one adjacent target.<br>Save: Adjacent target may make a DC 20 Dexterity save to negate.<br>Special: If there are no adjacent targets, deal bonus damage to original target.", "And Stay Down!<br>Target suffers maximum damage and is knocked prone.<br>Save: DC 20 Constitution negates; deals bonus damage instead.<br>Recover: A move-equivalent action is required to stand from prone. This provokes attacks of opportunity from threatening opponents.", "Armor Dent<br>Target suffers maximum damage and attack also deals damage to target’s armor (hardness applies).<br>Save: DC 20 Dexterity negates; deals bonus damage to the target instead.<br>Special: If target does not wear armor, damage is dealt to shield or deal additional bonus damage again to target instead.", "Back Breaker<br>Target suffers maximum plus bonus damage and 1d4 Dex damage.<br>Save: DC 20 Constitution negates; deals additional bonus damage instead.<br>Heal: Rest or restoration magic.", "Bell Ringer<br>Target suffers maximum damage, 1 Int damage and is sickened for 1 round.<br>Save: DC 20 Constitution negates; deals bonus damage instead.<br>Heal: A DC 20 Heal check removes the sickened condition. Rest or restoration magic is needed to heal ability damage.", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+
+    const randomNumber = getRandomInt(50);
+
+    const title = g_melee[randomNumber];
+
+    $("#text").prepend(title + "<br><br><br>");
     $("#text").show();
-
-    const g_melee = ["2 For 1", "And Stay Down!", "Armor Dent", "Back Breaker", "Bell Ringer"];
-
-    const randomNumber = getRandomInt(5);
-
-    $("#text").prepend(g_melee[randomNumber] + "<br>");
-
     event.preventDefault();
   });
 });
@@ -20,3 +20,5 @@ $(document).ready(function() {
 // Might straight up make 16 arrays
 
 // One each for each table
+
+// Use break tags to have one array element with 4 lines. This will get long lol
